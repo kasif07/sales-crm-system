24bbbd1bb25a6350cebd352fd17e073f2055d449
kasif07 <kashifazad9115@gmail.com>
2026-01-18 02:59:25 +0530

    Update frontend API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://sales-crm-system.onrender.com/api"
+
 });
 
 // 🔐 Automatically attach token
