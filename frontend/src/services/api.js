161a37a50bd6716ed2903ae28c83205b2ab5c15f
kasif07 <kashifazad9115@gmail.com>
2026-01-18 14:36:12 +0530

    Fix react-router, connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sales-crm-system.onrender.com/api"
 });
 
 // Token automatically attach karega
